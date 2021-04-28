9127895820c0f992a6e8a6052bfd64233999956e
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-04-28 14:34:51 +0200

    Add captureException on the server
    It has an optional `isHandled` parameter, controlling whether the event should be handled

--- packages/nextjs/src/index.server.ts
+++ packages/nextjs/src/index.server.ts
@@ -1,4 +1,5 @@
-import { configureScope, init as nodeInit } from '@sentry/node';
+import { captureException as captureNodeException, configureScope, init as nodeInit, withScope } from '@sentry/node';
+import { CaptureContext, Event } from '@sentry/types';
 
 import { MetadataBuilder } from './utils/metadataBuilder';
 import { NextjsOptions } from './utils/nextjsOptions';
@@ -24,6 +25,21 @@ export function init(options: NextjsOptions): void {
   configureScope(scope => {
     scope.setTag('runtime', 'node');
   });
+}
+
+export function captureException(exception: any, captureContext?: CaptureContext, isHandled: boolean = false): string {
+  withScope(scope => {
+    scope.addEventProcessor((event: Event) => {
+      if (event && event.exception && event.exception.values) {
+        event.exception.values[0].mechanism = {
+          handled: isHandled,
+          type: 'serverError',
+        };
+      }
+      return event;
+    });
+  });
+  return captureNodeException(exception, captureContext);
 }
 
 export { withSentryConfig } from './utils/config';
